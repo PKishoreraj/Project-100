ea39c0603ac795ed970e47bc4444e859dd73e1ca
PKishoreraj <113040359+PKishoreraj@users.noreply.github.com>
2023-01-18 23:12:45 +0530

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,8 +1,13 @@
-// Your web app's Firebase configuration
-//ADD YOUR FIREBASE LINKS HERE
-
-
-// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDsUrMWpmozmuyYk4gPuB0PwdcmctnpQTc",
+    authDomain: "lets-meet-74cf9.firebaseapp.com",
+    projectId: "lets-meet-74cf9",
+    storageBucket: "lets-meet-74cf9.appspot.com",
+    messagingSenderId: "688289518236",
+    appId: "1:688289518236:web:ccf0d24a075fdf88269359"
+  };
+  
+  const app = initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 
 
